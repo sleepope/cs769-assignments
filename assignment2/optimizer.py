e6142a689cf7993049e40c69c67cabf23a42a5d5
sleepope <SciZeal@outlook.com>
2022-03-18 01:30:48 -0500

    optimizer check passed 🎉

--- assignment2/optimizer.py
+++ assignment2/optimizer.py
@@ -38,23 +38,48 @@ class AdamW(Optimizer):
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
-                raise NotImplementedError()
-
                 # State should be stored in this dictionary
                 state = self.state[p]
+                # State init
+                if len(state) == 0:
+                    state["step"] = 0                                    # t
+                    state["exp_avg_grad"] = torch.zeros_like(p.data)     # m_0
+                    state["exp_avg_grad_sq"] = torch.zeros_like(p.data)  # t_0
+
+                state["step"] += 1
 
                 # Access hyperparameters from the `group` dictionary
                 alpha = group["lr"]
 
                 # Update first and second moments of the gradients
+                m = state["exp_avg_grad"]
+                v = state["exp_avg_grad_sq"]
+                beta1, beta2 = group["betas"]
+
+                # BUG: why not work???
+                # m = beta1 * m + (1-beta1) * grad
+                # v = beta2 * v + (1-beta2) * grad**2
+                m *= beta1
+                m += (1 - beta1) * grad
+                v *= beta2
+                v += (1 - beta2) * grad**2
 
                 # Bias correction
                 # Please note that we are using the "efficient version" given in
                 # https://arxiv.org/abs/1412.6980
+                if group["correct_bias"]:
+                    m_hat = m / (1 - beta1 ** state["step"])
+                    v_hat = v / (1 - beta2 ** state["step"])
 
                 # Update parameters
+                    p.data -= (alpha * m_hat / (v_hat**0.5 + group["eps"]))
+                else:
+                    p.data -= (alpha * m / (v**0.5 + group["eps"]))
+
 
                 # Add weight decay after the main gradient-based updates.
+                # p.data *= (1 - alpha * group["weight_decay"]) # BUG: why not work???
+                p.data = p.data - p.data * alpha * group["weight_decay"]
                 # Please note that the learning rate should be incorporated into this update.
 
         return loss
